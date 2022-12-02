8b447c605da755ad8ffa2eafc1179c9d21b09e7e
Darek79 <darius.kloda@gmail.com>
2022-12-02 12:56:54 +0100

    prod-fix

--- src/utils/fetchFromDB.js
+++ src/utils/fetchFromDB.js
@@ -3,10 +3,10 @@ export default async function fetchFromDB(url) {
         const data = await fetch(url, {
             headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
         });
-        if (data.statusText !== 'OK') {
-            throw new Error('Something went wrong please try again later');
+        if (data.status === 200 || data.status === 304) {
+            return data.json();
         }
-        return data.json();
+        throw new Error('Something went wrong please try again later');
     } catch (error) {
         if (error) {
             return error.message;
